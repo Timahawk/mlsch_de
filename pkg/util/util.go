0f03abb12fe87a8ef0da344ed4e6a47e6378d15c
Tim Wendel <mit.ende@mailbox.org>
2022-04-11 22:14:14 +0200

    First draft with bugs.

---
+++ pkg/util/util.go
@@ -0,0 +1,33 @@
+package util
+
+import "math/rand"
+
+const (
+	// This are all the letters RandString can draw from.
+	letterBytes   = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+// Generates Random String with Lenght n from Letters specified in letterBytes
+//
+// Stolen from Stack Overflow.
+// https://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-go
+func RandString(n int) string {
+	b := make([]byte, n)
+	// A rand.Int63() generates 63 random bits, enough for letterIdxMax letters!
+	for i, cache, remain := n-1, rand.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = rand.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
+}
